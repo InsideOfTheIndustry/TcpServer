35b0f524e4781a60dc88ed66c104c84dc773915e
zhujunjie <zhujunjie@egova.com.cn>
2021-06-04 17:19:28 +0800

    fix: initxormengine err handle

--- main.go
+++ main.go
@@ -21,9 +21,12 @@ import (
 
 func main() {
 
-	config.Setup("./config/config.yaml")                    // 读取配置文件
-	logServer.Setup("info")                                 // 设置日志等级
-	xormdatabase.InitXormEngine()                           // 初始化xorm引擎
+	config.Setup("./config/config.yaml") // 读取配置文件
+	logServer.Setup("info")              // 设置日志等级
+	// 初始化xorm引擎
+	if err := xormdatabase.InitXormEngine(); err != nil {
+		logServer.Error("初始化xorm引擎失败: %s", err.Error())
+	}
 	redisdatabase.InitRedis()                               // 初始化reds
 	ctx, cancel := context.WithCancel(context.Background()) // 全局上下文控制
 	defer cancel()
