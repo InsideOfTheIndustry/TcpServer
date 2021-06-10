ff97cd285e55e6880c596d19a6dd3a0745cd5951
zhujunjie <zhujunjie@egova.com.cn>
2021-06-10 22:21:44 +0800

    delete: test

--- server/server_test.go
+++ server/server_test.go
@@ -1,46 +1,35 @@
 package server
 
-import (
-	"context"
-	"testing"
-	"time"
+// func TestNewtcp(t *testing.T) {
 
-	"github.com/InsideOfTheIndustry/TcpServe/config"
-	redisdatabase "github.com/InsideOfTheIndustry/TcpServe/database/redis"
-	"github.com/InsideOfTheIndustry/TcpServe/database/xormdatabase"
-	"github.com/InsideOfTheIndustry/TcpServe/logServer"
-)
+// 	logServer.Setup("info")               // 设置日志等级
+// 	config.Setup("../config/config.yaml") // 读取配置文件
+// 	// 初始化xorm引擎
+// 	if err := xormdatabase.InitXormEngine(); err != nil {
+// 		logServer.Error("初始化xorm引擎失败: %s", err.Error())
+// 	}
+// 	redisdatabase.InitRedis()                               // 初始化reds
+// 	ctx, cancel := context.WithCancel(context.Background()) // 全局上下文控制
+// 	// defer cancel()
+// 	tcpserver, err := NewTcpServer(ctx) // 启动tcp服务
+// 	if err != nil {
+// 		t.Errorf("启动tcp失败：%s", err.Error())
+// 	}
+// 	var count = 0
+// 	for count < 10 {
+// 		time.Sleep(time.Millisecond * 1000)
+// 		count += 1
+// 	}
 
-func TestNewtcp(t *testing.T) {
+// 	cancel()
+// 	<-ctx.Done()
+// 	time.Sleep(time.Microsecond * 1000 * 50)
+// 	for i := range tcpserver.conn {
+// 		_, err = tcpserver.conn[i].Write([]byte("s"))
+// 		if err == nil {
+// 			t.Error("有连接未关闭")
+// 		}
+// 	}
 
-	logServer.Setup("info")               // 设置日志等级
-	config.Setup("../config/config.yaml") // 读取配置文件
-	// 初始化xorm引擎
-	if err := xormdatabase.InitXormEngine(); err != nil {
-		logServer.Error("初始化xorm引擎失败: %s", err.Error())
-	}
-	redisdatabase.InitRedis()                               // 初始化reds
-	ctx, cancel := context.WithCancel(context.Background()) // 全局上下文控制
-	// defer cancel()
-	tcpserver, err := NewTcpServer(ctx) // 启动tcp服务
-	if err != nil {
-		t.Errorf("启动tcp失败：%s", err.Error())
-	}
-	var count = 0
-	for count < 10 {
-		time.Sleep(time.Millisecond * 1000)
-		count += 1
-	}
-
-	cancel()
-	<-ctx.Done()
-	time.Sleep(time.Microsecond * 1000 * 50)
-	for i := range tcpserver.conn {
-		_, err = tcpserver.conn[i].Write([]byte("s"))
-		if err == nil {
-			t.Error("有连接未关闭")
-		}
-	}
-
-	logServer.Info("Tcp服务停止")
-}
+// 	logServer.Info("Tcp服务停止")
+// }
